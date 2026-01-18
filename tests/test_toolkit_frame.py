5e47daecfa6db0776e36462b60ba3319cd088164
Brianna Rainey <brianna@tassaron.com>
2026-01-18 01:19:12 -0500

    test FloodFrame function

--- tests/test_toolkit_frame.py
+++ tests/test_toolkit_frame.py
@@ -1,6 +1,14 @@
 import numpy
-from avp.toolkit.frame import BlankFrame
+from avp.toolkit.frame import BlankFrame, FloodFrame
 
 
 def test_blank_frame():
+    """BlankFrame creates a frame of all zeros"""
     assert numpy.asarray(BlankFrame(1920, 1080), dtype="int32").sum() == 0
+
+
+def test_flood_frame():
+    """FloodFrame given (1, 1, 1, 1) creates a frame of sum 1920 * 1080 * 4"""
+    assert numpy.asarray(FloodFrame(1920, 1080, (1, 1, 1, 1)), dtype="int32").sum() == (
+        1920 * 1080 * 4
+    )
