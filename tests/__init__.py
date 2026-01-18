0681f3d61db60d64eacb31c0d0a5fe11bbe47f75
Brianna Rainey <brianna@tassaron.com>
2026-01-18 01:18:39 -0500

    add MockVideoWorker and imageDataSum

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,20 +1,39 @@
 import os
+import numpy
+
+# core always has to be imported first
+import avp.core
+from avp.toolkit.ffmpeg import readAudioFile
 from pytest import fixture
-
-
-def getTestDataPath(filename):
-    tests_dir = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(tests_dir, "data", filename)
 
 
 @fixture
 def audioData():
-    from avp.toolkit.ffmpeg import readAudioFile
-
+    """Fixture that gives a tuple of (completeAudioArray, duration)"""
     soundFile = getTestDataPath("test.ogg")
-    yield readAudioFile(soundFile, None)
+    yield readAudioFile(soundFile, MockVideoWorker())
 
 
-class mockSignal:
+def getTestDataPath(filename):
+    """Get path to a file in the ./data directory"""
+    tests_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(tests_dir, "data", filename)
+
+
+class MockSignal:
+    """Pretends to be a pyqtSignal"""
+
     def emit(self, *args):
         pass
+
+
+class MockVideoWorker:
+    """Pretends to be a video thread worker"""
+
+    progressBarSetText = MockSignal()
+    progressBarUpdate = MockSignal()
+
+
+def imageDataSum(image):
+    """Get sum of raw data of a Pillow Image object"""
+    return numpy.asarray(image, dtype="int32").sum()
