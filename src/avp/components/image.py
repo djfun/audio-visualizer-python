3b91c14e105fd9d7f3e1c3a47e2b61f25ea273c5
Brianna Rainey <brianna@tassaron.com>
2026-01-21 20:04:42 -0500

    add respondToAudio option to Image component this causes the image to scale up and down slightly based on the input audio file

--- src/avp/components/image.py
+++ src/avp/components/image.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageOps, ImageEnhance
 from PyQt6 import QtWidgets
 import os
+from copy import copy
 
 from ..component import Component
 from ..toolkit.frame import BlankFrame
+from .original import Component as Visualizer
 
 
 class Component(Component):
@@ -12,6 +14,7 @@ class Component(Component):
 
     def widget(self, *args):
         super().widget(*args)
+
         self.page.pushButton_image.clicked.connect(self.pickImage)
         self.page.comboBox_resizeMode.addItem("Scale")
         self.page.comboBox_resizeMode.addItem("Cover")
@@ -27,6 +30,8 @@ class Component(Component):
                 "yPosition": self.page.spinBox_y,
                 "resizeMode": self.page.comboBox_resizeMode,
                 "mirror": self.page.checkBox_mirror,
+                "respondToAudio": self.page.checkBox_respondToAudio,
+                "sensitivity": self.page.spinBox_sensitivity,
             },
             presetNames={
                 "imagePath": "image",
@@ -37,16 +42,18 @@ class Component(Component):
         )
 
     def update(self):
-        if self.page.comboBox_resizeMode.currentIndex() == 0:
-            self.page.spinBox_scale.setEnabled(True)
-        else:
-            self.page.spinBox_scale.setEnabled(False)
+        self.page.spinBox_sensitivity.setEnabled(
+            self.page.checkBox_respondToAudio.isChecked()
+        )
+        self.page.spinBox_scale.setEnabled(
+            self.page.comboBox_resizeMode.currentIndex() == 0
+        )
 
     def previewRender(self):
-        return self.drawFrame(self.width, self.height)
+        return self.drawFrame(self.width, self.height, None)
 
     def properties(self):
-        props = ["static"]
+        props = ["pcm" if self.respondToAudio else "static"]
         if not os.path.exists(self.imagePath):
             props.append("error")
         return props
@@ -57,10 +64,49 @@ class Component(Component):
         if not os.path.exists(self.imagePath):
             return "The image selected does not exist!"
 
-    def frameRender(self, frameNo):
-        return self.drawFrame(self.width, self.height)
+    def preFrameRender(self, **kwargs):
+        super().preFrameRender(**kwargs)
+        if not self.respondToAudio:
+            return
 
-    def drawFrame(self, width, height):
+        smoothConstantDown = 0.08 + 0
+        smoothConstantUp = 0.8 - 0
+        self.lastSpectrum = None
+        self.spectrumArray = {}
+
+        for i in range(0, len(self.completeAudioArray), self.sampleSize):
+            if self.canceled:
+                break
+            self.lastSpectrum = Visualizer.transformData(
+                i,
+                self.completeAudioArray,
+                self.sampleSize,
+                smoothConstantDown,
+                smoothConstantUp,
+                self.lastSpectrum,
+                self.sensitivity,
+            )
+            self.spectrumArray[i] = copy(self.lastSpectrum)
+
+            progress = int(100 * (i / len(self.completeAudioArray)))
+            if progress >= 100:
+                progress = 100
+            pStr = "Analyzing audio: " + str(progress) + "%"
+            self.progressBarSetText.emit(pStr)
+            self.progressBarUpdate.emit(int(progress))
+
+    def frameRender(self, frameNo):
+        return self.drawFrame(
+            self.width,
+            self.height,
+            (
+                None
+                if not self.respondToAudio
+                else self.spectrumArray[frameNo * self.sampleSize]
+            ),
+        )
+
+    def drawFrame(self, width, height, dynamicScale):
         frame = BlankFrame(width, height)
         if self.imagePath and os.path.exists(self.imagePath):
             image = Image.open(self.imagePath)
@@ -79,8 +125,27 @@ class Component(Component):
                 newWidth = int((image.width / 100) * self.scale)
                 image = image.resize((newWidth, newHeight), Image.Resampling.LANCZOS)
 
+            # Respond to audio
+            scale = 0
+            if dynamicScale is not None:
+                scale = dynamicScale[36 * 4] / 4
+                image = ImageOps.contain(
+                    image,
+                    (
+                        image.width + int(scale / 2),
+                        image.height + int(scale / 2),
+                    ),
+                    Image.Resampling.LANCZOS,
+                )
+
             # Paste image at correct position
-            frame.paste(image, box=(self.xPosition, self.yPosition))
+            frame.paste(
+                image,
+                box=(
+                    self.xPosition - (0 if not self.respondToAudio else int(scale / 2)),
+                    self.yPosition - (0 if not self.respondToAudio else int(scale / 2)),
+                ),
+            )
             if self.rotate != 0:
                 frame = frame.rotate(self.rotate)
 
