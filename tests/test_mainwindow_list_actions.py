9a7e574b2713d161374921726cfee82b69b871d3
Brianna Rainey <brianna@tassaron.com>
2026-05-31 17:40:26 -0400

    test that drag-and-drop reorders components

--- tests/test_mainwindow_list_actions.py
+++ tests/test_mainwindow_list_actions.py
@@ -1,10 +1,9 @@
 """Tests of `actions.py` - MainWindow component list manipulation via undoable actions"""
 
 from PyQt6 import QtCore
-import os
-from pytest import fixture
+import pytest
 from pytestqt import qtbot
-from . import getTestDataPath, window
+from . import window
 
 
 def test_mainwindow_addComponent(qtbot, window):
@@ -50,3 +49,45 @@ def test_mainwindow_moveComponent_undo(qtbot, window):
     window.undoStack.undo()
     # check if 0 is still 0 after undo
     assert window.core.selectedComponents[1].ui != comp0
+
+
+def test_mainwindow_componentList_qrect_sizes(qtbot, window):
+    """Add two components to MainWindow's componentList and test QRect sizes"""
+    window.compMenu.actions()[0].trigger()
+    window.compMenu.actions()[1].trigger()
+    componentListItem0 = window.listWidget_componentList.model().index(0)
+    componentListItem1 = window.listWidget_componentList.model().index(1)
+    rect0x, rect0y, rect0w, rect0h = window.listWidget_componentList.visualRect(
+        componentListItem0
+    ).getRect()
+    rect1x, rect1y, rect1w, rect1h = window.listWidget_componentList.visualRect(
+        componentListItem1
+    ).getRect()
+
+    # only the y coordinate should be different between items in the component list
+    assert rect0x == rect1x
+    assert rect0y != rect1y
+    assert rect0w == rect1w
+    assert rect0h == rect1h
+
+
+@pytest.mark.parametrize("row, movement", ((0, 1), (1, 0)))
+def test_mainwindow_dragDropComponent(qtbot, window, row, movement):
+    """Add two components to MainWindow's componentList,
+    then call dragComponent with a fake QMouseEvent to simulate
+    drag-and-drop reordering components in the list"""
+    window.compMenu.actions()[0].trigger()
+    window.compMenu.actions()[1].trigger()
+    comp0 = window.core.selectedComponents[0].ui
+    rect0x, rect0y, rect0w, rect0h = window.listWidget_componentList.visualRect(
+        window.listWidget_componentList.model().index(0)
+    ).getRect()
+
+    class FakeEvent:
+        def position(self):
+            return QtCore.QPoint(rect0x, rect0y + (rect0h * movement))
+
+    window.listWidget_componentList.setCurrentRow(row)
+    assert window.core.selectedComponents[0].ui == comp0
+    window.dragComponent(FakeEvent())
+    assert window.core.selectedComponents[1].ui == comp0
