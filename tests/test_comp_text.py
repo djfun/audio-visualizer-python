a6d295df9ccb014ef42f8696203b38650a1e16c3
Brianna Rainey <brianna@tassaron.com>
2026-06-05 20:19:49 -0400

    test changing (x, y) with mouse click & center text button

--- tests/test_comp_text.py
+++ tests/test_comp_text.py
@@ -1,8 +1,9 @@
 from avp.command import Command
+from PyQt6 import QtCore
 from PyQt6.QtGui import QFont
 from pytestqt import qtbot
 from pytest import fixture, mark
-from . import audioData, command, MockSignal, imageDataSum
+from . import audioData, command, window, MockSignal, imageDataSum
 
 
 @fixture
@@ -10,6 +11,12 @@ def coreWithTextComp(qtbot, command):
     """Fixture providing a Command object with Title Text component added"""
     command.core.insertComponent(0, command.core.moduleIndexFor("Title Text"), command)
     yield command.core
+
+
+@fixture
+def windowWithTextComp(qtbot, window):
+    window.core.insertComponent(0, window.core.moduleIndexFor("Title Text"), window)
+    yield window
 
 
 def setTextSettings(comp):
@@ -49,3 +56,39 @@ def test_comp_text_alignment(coreWithTextComp, alignment):
     comp = coreWithTextComp.selectedComponents[0]
     comp.page.comboBox_textAlign.setCurrentIndex(alignment)
     assert comp.alignment == alignment
+
+
+@mark.parametrize("pos", ((0, 0), (23, 37), (900, 900)))
+def test_comp_text_previewClickEvent_dynamic_assertion(windowWithTextComp, pos):
+    comp = windowWithTextComp.core.selectedComponents[0]
+    comp.previewClickEvent(pos, (555, 333), QtCore.Qt.MouseButton.LeftButton)
+    assert comp.xPosition == comp.pixelValForAttr("xPosition")
+    assert comp.yPosition == comp.pixelValForAttr("yPosition")
+
+
+def test_comp_text_previewClickEvent_static_assertion(windowWithTextComp):
+    """Test without using pixelValForAttr method"""
+    comp = windowWithTextComp.core.selectedComponents[0]
+    comp.previewClickEvent((192, 108), (1920, 1080), QtCore.Qt.MouseButton.LeftButton)
+    assert comp.xPosition == 192
+    assert comp.yPosition == 108
+
+
+def test_comp_text_previewClickEvent_undo_redo(windowWithTextComp):
+    comp = windowWithTextComp.core.selectedComponents[0]
+    oldValue = comp.xPosition
+    comp.previewClickEvent((192, 108), (1920, 1080), QtCore.Qt.MouseButton.LeftButton)
+    windowWithTextComp.undoStack.undo()
+    assert comp.xPosition == oldValue
+    windowWithTextComp.undoStack.redo()
+    assert comp.xPosition == 192
+
+
+def test_comp_text_centerText_undo_redo(windowWithTextComp):
+    comp = windowWithTextComp.core.selectedComponents[0]
+    comp.page.spinBox_xTextAlign.setValue(0)
+    comp.page.pushButton_center.click()
+    windowWithTextComp.undoStack.undo()
+    assert comp.xPosition == 0
+    windowWithTextComp.undoStack.redo()
+    assert comp.xPosition == comp.pixelValForAttr("xPosition", 0.5)
